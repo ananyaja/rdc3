b9ea49250c1fcf841f4567a9da96ca07bfa25ab6
ananyaja <ananyaja19@gmail.com>
2020-12-28 11:25:39 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 14:33:13 2020
+
+@author: User1
+"""
+
+import streamlit as st
+
+st.title('Hello streamlit')
+st.subheader('welcome to first streamlit')
+st.text('ananyaja')
+
